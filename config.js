83c60a530f6910e139e153196f08bbfc01390e09
Herokumine <whatsappbot11000@gmail.com>
2024-08-23 13:12:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID || "",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/cad7038fe82e47f79c609.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "👍👉DILISHA-MD IS START 👈🤟",
 AUTO_READ_STATUES: process.env.AUTO_READ_STATUES ||"true",
