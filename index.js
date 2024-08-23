d7d302c70a8780345e6bf8aeee533d0be739e861
Herokumine <whatsappbot11000@gmail.com>
2024-08-23 13:52:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ const port = process.env.PORT || 8000;
 //=============================================
 
 async function connectToWA() {
-console.log("Connecting DILISHA-MD...");
+console.log("Connecting DILISHA-MD🥰...");
 const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/auth_info_baileys/')
 var { version } = await fetchLatestBaileysVersion()
 
