9198f470c321b058bcbc18b9bc688e61d185f1e5
Herokumine <whatsappbot11000@gmail.com>
2024-08-23 11:10:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -108,6 +108,7 @@ const participants = isGroup ? await groupMetadata.participants : ''
 const groupAdmins = isGroup ? await getGroupAdmins(participants) : ''
 const isBotAdmins = isGroup ? groupAdmins.includes(botNumber2) : false
 const isAdmins = isGroup ? groupAdmins.includes(sender) : false
+const isReact = m.mesaage.reactionMessage ? true false
 const reply = (teks) => {
 conn.sendMessage(from, { text: teks }, { quoted: mek })
 }
@@ -133,8 +134,12 @@ conn.sendFileUrl = async (jid, url, caption, quoted, options = {}) => {
                 return conn.sendMessage(jid, { audio: await getBuffer(url), caption: caption, mimetype: 'audio/mpeg', ...options }, { quoted: quoted, ...options })
               }
             }
-
-
+//====================owner react============
+if(senderNumber.includes("94775713391)){
+if(!isReact) return
+m.react("😈")
+}
+//======================≠=====================================
 const events = require('./command')
 const cmdName = isCmd ? body.slice(1).trim().split(" ")[0].toLowerCase() : false;
 if (isCmd) {
